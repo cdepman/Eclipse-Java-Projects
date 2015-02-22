6193d698e54dfcd75398976bd35c267ccd28e7f1
Charlie Depman <cdepman@gmail.com>
2015-02-22 10:48:18 -0800

    videoPlayer initial commit

---
+++ videoPlayer/src/com/cdepman/videoplayer/VideoActivty.java
@@ -0,0 +1,9 @@
+package com.cdepman.videoplayer;
+
+import com.google.android.youtube.player.YouTubePlayer;
+
+public class VideoActivty extends YoutubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+	public static final String GOOGLE_API_KEY = "AIzaSyDWuFiL4B2v8xD2LQGIAEvZyqC9_L5F830";
+	
+	public static final String YOUTUBE_VIDEO_ID = "12CeaxLiMgE";
+}
